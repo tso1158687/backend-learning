c99adf22b9b9ffaeb7824fb2949278dc210157cd
jason zheng <tso1158687@gmail.com>
2026-07-16 23:52:12 +0800

    add find one and error handler

--- ecommerce-nestjs/src/products/products.controller.ts
+++ ecommerce-nestjs/src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
@@ -7,5 +7,10 @@ export class ProductsController {
   @Get()
   findAll() {
     return this.productsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.productsService.findOne(id);
   }
 }
--- ecommerce-nestjs/src/products/products.service.ts
+++ ecommerce-nestjs/src/products/products.service.ts
@@ -1,12 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProductsService {
-    constructor(
-        private readonly prismaService: PrismaService
-    ) {}
+  constructor(private readonly prismaService: PrismaService) {}
   findAll() {
     return this.prismaService.product.findMany();
+  }
+
+  async findOne(id: string) {
+    const product = await this.prismaService.product.findUnique({
+      where: { id },
+    });
+    if (!product) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+    return product;
   }
 }
